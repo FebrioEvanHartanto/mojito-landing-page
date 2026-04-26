998fb9c69f5d46f778e4be5a32beb4551654e26f
Rio <febrioevanhartanto@gmail.com>
2026-04-26 20:07:22 +0800

    Initial Setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import gsap from 'gsap';
+import {ScrollTrigger, SplitText} from "gsap/all";
+gsap.registerPlugin(ScrollTrigger,SplitText);
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
+    </div>
+  );
+}
