d1b062065594a47d975fe8299359c3c4d2b9a099
Rio <febrioevanhartanto@gmail.com>
2026-04-27 16:17:19 +0800

    Create hero, implement GSAP to text and headings

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,15 @@ import {ScrollTrigger, SplitText} from "gsap/all";
 gsap.registerPlugin(ScrollTrigger,SplitText);
 import Image from "next/image";
 import Navbar from "@/app/components/Navbar";
+import Hero from "@/app/components/Hero";
 
 
 export default function Home() {
   return (
    <main>
        <Navbar />
+       <Hero />
+       <div className="h-dvh bg-black"></div>
    </main>
   );
 }
